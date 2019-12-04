b218e5096dfc54f6d501420301b2bd4310f115ed
Nicholas Claudio <nclaudio449@gmail.com>
2019-12-04 13:06:38 -0600

    updated the client to fetch and display post data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,22 +8,12 @@ import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
-    data: null,
+    posts: [],
     token: null,
     user: null
-  }
+  };
 
   componentDidMount() {
-    axios.get('http://localhost:5000')
-      .then((response) => {
-        this.setState({
-          data: response.data
-        })
-      })
-      .catch((error) => {
-        console.error(`Error fetching data: ${error}`);
-      })
-
       this.authenticateUser();
   }
 
@@ -41,10 +31,15 @@ class App extends React.Component {
           'x-auth-token': token
         }
       }
+
+
       axios.get('http://localhost:5000/api/auth', config)
         .then((response) => {
           localStorage.setItem('user', response.data.name)
-          this.setState({ user: response.data.name })
+          this.setState({ user: response.data.name, token },
+            () => {
+              this.loadData();
+            })
         })
         .catch((error) => {
           localStorage.removeItem('user');
@@ -52,6 +47,32 @@ class App extends React.Component {
           console.error(`Error logging in: ${error}`);
         })
     }
+    
+  }
+
+  loadData = () => {
+    const { token } = this.state;
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+      axios.get('http://localhost:5000', config)
+      .then((response) => {
+        this.setState({
+          posts: response.data
+        })
+      })
+      .catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+    }
+
+
+    
+
   }
 
   logOut = () => {
@@ -61,7 +82,7 @@ class App extends React.Component {
   }
 
   render() {
-    let { user, data } = this.state;
+    let { user, posts } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     }
@@ -87,16 +108,23 @@ class App extends React.Component {
             </ul>
             </header>
             <main>
-                <Route exacr path="/">
-                  {user ?
+                <Route exact path="/">
+                  {user ? (
                     <React.Fragment>
                       <div>Hello {user}!</div>
-                      <div>{data}</div>
-                    </React.Fragment> :
+                      <div>{posts.map(post =>(
+                        <div key={post._id}>
+                          <h1>{post.title}</h1>
+                          <p>{post.body}</p>
+                        </div>
+                      ))}
+                      </div>
+                  </React.Fragment> 
+                  ) : (
                     <React.Fragment>
                       Please Register or Login
                     </React.Fragment>
-                  }
+                  )}
                 </Route>
                 <Switch>
                   <Route        
