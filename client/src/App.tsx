7700abf40eb33ee7d6cf281da1bea8ff81035d9c
Nicholas Claudio <nclaudio449@gmail.com>
2019-12-04 19:12:10 -0600

    Completed Activity 11

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,7 +38,7 @@ class App extends React.Component {
       }
 
 
-      axios.get('http://localhost:5000/api/auth', config)
+      axios.get('api/auth', config)
         .then((response) => {
           localStorage.setItem('user', response.data.name)
           this.setState({ user: response.data.name, token },
@@ -64,7 +64,7 @@ class App extends React.Component {
           'x-auth-token': token
         }
       };
-      axios.get('http://localhost:5000/api/posts', config)
+      axios.get('api/posts', config)
       .then((response) => {
         this.setState({
           posts: response.data
@@ -104,7 +104,7 @@ class App extends React.Component {
       };
 
       axios
-        .delete(`http://localhost:5000/api/posts/${post._id}`, config)
+        .delete(`api/posts/${post._id}`, config)
         .then(response => {
           const newPosts = this.state.posts.filter(p => p._id !== post._id)
           this.setState({
